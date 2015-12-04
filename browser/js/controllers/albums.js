d704a723dd4ac144ae7f5bd003365ca73af9c334
Sam Narisi <snarisi@gmail.com>
2015-12-04 15:31:44 -0500

    Add albums and artists state

--- browser/js/controllers/albums.js
+++ browser/js/controllers/albums.js
@@ -3,13 +3,13 @@ app.controller('AlbumsCtrl', function ($scope, $rootScope, AlbumFactory) {
 	.then(function (albums) {
 		$scope.albums = albums;
 	});
-	$rootScope.$on('changeView', function (evt, data) {
-		$scope.showMe = (data.name == 'allAlbums');
-	});
-	$scope.viewAlbum = function (albumId) {
-		$rootScope.$broadcast('changeView', {
-			name: 'oneAlbum',
-			id: albumId
-		});
-	};
+//	$rootScope.$on('changeView', function (evt, data) {
+//		$scope.showMe = (data.name == 'allAlbums');
+// 	});
+//	$scope.viewAlbum = function (albumId) {
+//		$rootScope.$broadcast('changeView', {
+//			name: 'oneAlbum',
+//			id: albumId
+//		});
+//	};
 });
--- browser/js/module.js
+++ browser/js/module.js
@@ -1,1 +1,16 @@
-var app = angular.module('juke', []);
+var app = angular.module('juke', ['ui.router']);
+
+app.config(function($stateProvider) {
+
+  $stateProvider.state('albums', {
+    url: '/albums',
+    templateUrl: '/albums.html',
+    controller: 'AlbumsCtrl'
+  });
+    
+  $stateProvider.state('artists', {
+    url: '/artists',
+    templateUrl: '/artists.html',
+    controller: 'ArtistsCtrl'
+  });
+});
