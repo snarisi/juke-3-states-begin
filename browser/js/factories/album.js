ac9b7b9b3ccfe3116b4b438f29ffb13fa7dc6857
Sam Narisi <snarisi@gmail.com>
2015-12-05 13:16:33 -0500

    Add album cache

--- browser/js/factories/album.js
+++ browser/js/factories/album.js
@@ -1,15 +1,32 @@
-app.factory('AlbumFactory', function ($http, SongFactory) {
+app.factory('AlbumFactory', function ($http, SongFactory, Cache, $q) {
 	var AlbumFactory = {};
+  
+    var albumCache = {};
+  
 	AlbumFactory.fetchAll = function () {
+      
+        //clean the cache
+        Cache.clean(albumCache, 1)
+        
+        //return a promise for the cache if the cache exists
+        if (albumCache.all) return Cache.returnCache(albumCache.all);
+        
 		return $http.get('/api/albums')
 		.then(function (response) {
 			return response.data;
 		})
 		.then(function (albums) {
-			return albums.map(AlbumFactory.convert);
+            albums = albums.map(AlbumFactory.convert);
+            albumCache.all = albums;
+			return albums;
 		});
 	};
 	AlbumFactory.fetchById = function (id) {
+      
+        Cache.clean(albumCache, 1);
+      
+        if (albumCache[id]) return Cache.returnCache(albumCache[id]);  
+      
 		return $http.get('/api/albums/' + id)
 		.then(function (response) {
 			return response.data;
@@ -19,6 +36,7 @@ app.factory('AlbumFactory', function ($http, SongFactory) {
 			album.songs = album.songs.map(function (s) {
 				return SongFactory.convert(s, album.artists);
 			});
+            albumCache[id] = album;
 			return album;
 		});
 	};
